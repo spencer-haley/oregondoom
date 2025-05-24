a638da94c1c3dd19ddeae4491e174e9b003e6edb
Spencer Haley <spencer.haley@gmail.com>
2025-05-24 11:13:50 -0700

    Text Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
 import { Navigate } from 'react-router-dom';
 import EventsPage from './pages/EventsPage';
 import ReleasesPage from './pages/ReleasesPage';
